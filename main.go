e712bd4f3a4321ab2767eefc53a83983b2b9b476
reation <reation_11@163.com>
2020-11-23 22:34:16 +0800

    tijiao

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"routers"
+	"time"
+)
 
 func main(){
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context){
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+	router := routers.NewRouter()
 
-	r.Run()
+	s := &http.Server{
+		Addr					: ":8080",
+		Handler					: router,
+		ReadHeaderTimeout		: 10 * time.Second,
+		WriteTimeout			: 10 * time.Second,
+		MaxHeaderBytes			: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
