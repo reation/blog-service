ced2239075b06e73765c6d1a113f2d5db23d70e2
reation <reation_11@163.com>
2020-11-26 22:58:02 +0800

    tijiao

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-programming-tour-book/blog-service/global"
 )
 
 type Tag struct {}
@@ -11,7 +12,9 @@ func NewTag() Tag{
 }
 
 func (t Tag) Get (c *gin.Context){}
-func (t Tag) List (c *gin.Context){}
+func (t Tag) List (c *gin.Context){
+	c.JSON(200, gin.H{"detail" : global.DatabaseSetting.Password})
+}
 func (t Tag) Create (c *gin.Context){}
 func (t Tag) Update (c *gin.Context){}
 func (t Tag) Delete (c *gin.Context){}
--- main.go
+++ main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/internal/model"
 	"github.com/go-programming-tour-book/blog-service/internal/routers"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
 
+func init(){
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
 func main(){
-	fmt.Printf("This Is Create Tag Api")
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr					: ":8080",
+		Addr					: ":" + global.ServerSetting.HttpPort,
 		Handler					: router,
-		ReadHeaderTimeout		: 10 * time.Second,
-		WriteTimeout			: 10 * time.Second,
+		ReadHeaderTimeout		: global.ServerSetting.ReadTimeOut,
+		WriteTimeout			: global.ServerSetting.WriteTimeOut,
 		MaxHeaderBytes			: 1 << 20,
 	}
 
 	s.ListenAndServe()
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupSetting() error{
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeOut 	*= time.Second
+	global.ServerSetting.WriteTimeOut 	*= time.Second
+
+	return nil
 }
